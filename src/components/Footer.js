cbbb5c03ad9c01ca864fd5cbb6a9255264632432
Christian Scarlett <christianscarlett21@gmail.com>
2021-11-28 23:59:00 -0500

    footer touchups

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     return (
       <>
       <div className='footer'>
-        <p>© 2021 Christian Scarlett. Please leave feedback or bug reports in this <a href='https://forms.gle/HvKSaiyvodTGc1jH6'>form</a>.</p>
+        <p>Created 2021 by Christian Scarlett, Diana Ma, and anon. Please leave feedback or bug reports in this <a href='https://forms.gle/HvKSaiyvodTGc1jH6' target='_blank' rel="noreferrer">form</a>.</p>
       </div>
       </>
     );
